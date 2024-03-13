c7ef644911b662962b2170c11a1ac5ce30d16361
NikitaIlinov18 <158342700+NikitaIlinov18@users.noreply.github.com>
2024-03-14 00:09:25 +0300

    Update main.js

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     document.addEventListener('click', burgerInit)
 
@@ -8,6 +8,7 @@
         const burgerNavLink = e.target.closest('.nav__link')
 
         if (!burgerIcon && !burgerNavLink) return
+        if (document.documentElement.clientWidth > 900) return
 
         if (burgerIcon) {
             e.preventDefault()
@@ -19,4 +20,27 @@
             document.body.classList.remove('body--opened-menu')
         }
     }
+
+    const modal = document.querySelector('.modal')
+    const modalButton = document.querySelector('.about__img-button')
+
+    modalButton.addEventListener('click', openModal)
+    modal.addEventListener('click', closeModal)
+
+    function openModal(e) {
+        e.preventDefault()
+        document.body.classList.toggle('body--opened-modal')
+    }
+
+    function closeModal(e) {
+        e.preventDefault()
+
+        const target = e.target
+
+        if (target.closest('.modal__cancel') || target.classList.contains('modal')) {
+            document.body.classList.remove('body--opened-modal')
+        }
+
+    }
+
 })()
