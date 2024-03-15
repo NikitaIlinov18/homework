62152b86967f75b428cbbf0dac2514bbf063143a
NikitaIlinov18 <158342700+NikitaIlinov18@users.noreply.github.com>
2024-03-16 01:26:25 +0300

    Update main.js

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -1,5 +1,7 @@
 (function () {
 
+    // Burger
+
     document.addEventListener('click', burgerInit)
 
     function burgerInit(e) {
@@ -21,6 +23,9 @@
         }
     }
 
+
+    // Modals
+
     const modal = document.querySelector('.modal')
     const modalButton = document.querySelector('.about__img-button')
 
@@ -41,6 +46,37 @@
             document.body.classList.remove('body--opened-modal')
         }
 
+    }
+
+
+    // Tabs
+
+    const tabControls = document.querySelector('.tab-controls')
+
+    tabControls.addEventListener('click', toggleTab)
+
+    function toggleTab(e) {
+        
+        const tabControl = e.target.closest('.tab-controls__link')
+
+        if (!tabControl) return
+        e.preventDefault()
+        if (tabControl.classList.contains('tab-controls__link--active')) return
+
+        const tabContentID = tabControl.getAttribute('href')
+        const tabContent = document.querySelector(tabContentID)
+        const activeControl = document.querySelector('.tab-controls__link--active')
+        const activeContent = document.querySelector('.tab-content--show')
+
+        if (activeControl) {
+            activeControl.classList.remove('tab-controls__link--active')
+        }
+        if (activeContent) {
+            activeContent.classList.remove('tab-content--show')
+        }
+        
+        tabControl.classList.add('tab-controls__link--active')
+        tabContent.classList.add('tab-content--show')
     }
 
 })()
