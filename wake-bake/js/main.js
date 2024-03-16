873ff2bbd88795ecded0a7e948dda47146b34d5c
NikitaIlinov18 <158342700+NikitaIlinov18@users.noreply.github.com>
2024-03-17 00:50:57 +0300

    Update main.js

--- wake-bake/js/main.js
+++ wake-bake/js/main.js
@@ -56,7 +56,7 @@
     tabControls.addEventListener('click', toggleTab)
 
     function toggleTab(e) {
-        
+
         const tabControl = e.target.closest('.tab-controls__link')
 
         if (!tabControl) return
@@ -74,9 +74,44 @@
         if (activeContent) {
             activeContent.classList.remove('tab-content--show')
         }
-        
+
         tabControl.classList.add('tab-controls__link--active')
         tabContent.classList.add('tab-content--show')
     }
+
+
+    // Accordions
+
+    const accordionLists = document.querySelectorAll('.accordion-list')
+
+    accordionLists.forEach(el => {
+
+        el.addEventListener('click', (e) => {
+
+            const accordionList = e.currentTarget
+            const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+            const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__content')
+
+            const accordionControl = e.target.closest('.accordion-list__control');
+            if (!accordionControl) return
+            const accordionItem = accordionControl.parentElement;
+            const accordionContent = accordionControl.nextElementSibling;
+
+            if (accordionOpenedItem && accordionItem != accordionOpenedItem) {
+                accordionOpenedItem.classList.remove('accordion-list__item--opened');
+                accordionOpenedContent.style.maxHeight = null;
+            }
+
+            accordionItem.classList.toggle('accordion-list__item--opened');
+
+            if (accordionItem.classList.contains('accordion-list__item--opened')) {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+            } else {
+                accordionContent.style.maxHeight = null;
+            }
+
+        });
+
+    });
 
 })()
